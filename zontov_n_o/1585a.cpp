348158c52134036385d269a4fe5e67940f9b57e5
Nikita Zontov <70718862+zoLikeCode@users.noreply.github.com>
2022-10-16 00:19:34 +0300

    Код обновлен до полного решения

--- zontov_n_o/1585a.cpp
+++ zontov_n_o/1585a.cpp
@@ -22,21 +22,20 @@ void solve()
         }
         return;
     }
-
-    for (int i = 1; i < n; i += 1) {
-        if (m[i-1] == 1 && m[i] == 1) {
-            height += 5;
-        }
-        if (m[i-1] == 0 && m[i] == 0){
-            std::cout << -1 << std::endl;
-            return;
-        }
-        if (m[i-1] == 1 && m[i] == 0) {
-            height += 1;
-        }
-        if (m[i-1] == 0 && m[i] == 1) {
-            height += 0;
-        }
+    for (int i = 0; i < n; i += 1) {
+       if (m[i] == 1) {
+           height += 1;
+       }
+       if (i > 0 && m[i] == 1 && m[i-1] == 1) {
+           height += 4;
+       }
+       if (i > 0 && m[i] == 0 && m[i-1] == 0) {
+           std::cout << -1 << std::endl;
+           return;
+       }
+       if (m[i] == 0) {
+           height += 0;
+       }
     }
     std::cout << height << std::endl;
 }
