d945772d4f62d643d9e4a63c84218e7cf09d004f
Nikita Zontov <70718862+zoLikeCode@users.noreply.github.com>
2022-10-16 01:01:19 +0300

    Слон нашелся

--- zontov_n_o/1692c.cpp
+++ zontov_n_o/1692c.cpp
@@ -1,3 +1,33 @@
-//
-// Created by Nikita on 11.10.2022.
-//
+#include <iostream>
+#include <string>
+
+void solve(){
+
+    std::string m[8];
+    for (int iRow = 0; iRow < 8; iRow += 1) {
+        std::cin >> m[iRow];
+    }
+
+    for (int iRow = 1; iRow < 7; iRow += 1) {
+        for (int iCol = 1; iCol < 7; iCol += 1) {
+            if (m[iRow][iCol] == '#') {
+                if(m[iRow - 1][iCol + 1] == '#' && m[iRow + 1][iCol + 1] == '#') {
+                    std::cout << iRow + 1 << " " << iCol + 1 << std::endl;
+                    return;
+                }
+                if (m[iRow - 1][iCol - 1] == '#' && m[iRow + 1][iCol - 1] == '#') {
+                    std::cout << iRow + 1 << " " << iCol + 1 << std::endl;
+                    return;
+                }
+            }
+        }
+    }
+
+}
+
+int main() {
+    int tt = 0;
+    std::cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
